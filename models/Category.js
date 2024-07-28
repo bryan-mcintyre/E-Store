ccf7fd076ebcde2257bd7c816d30aadbede3a137
bryan-mcintyre <167257103+bryan-mcintyre@users.noreply.github.com>
2024-07-28 13:39:53 -0500

    Updated models and routes as of 07/28/24 13:40

--- models/Category.js
+++ models/Category.js
@@ -13,7 +13,7 @@ Category.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    categore_name: {
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
